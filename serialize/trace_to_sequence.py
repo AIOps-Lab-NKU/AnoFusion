519b36555aabc972028f05c43d4eeb27887c8ae2
zcyyc <43594100+zcyyc@users.noreply.github.com>
2022-01-13 11:27:16 +0800

    Update trace_to_sequence.py

--- serialize/trace_to_sequence.py
+++ serialize/trace_to_sequence.py
@@ -28,11 +28,11 @@ def trace_to_seq(df, start_time, end_time):
         # span_data = np.array(span_data)
         if len(span_data) != 0:
             span = span_data[0]
-            span_mean = np.mean(span_data) # 计算均值
-            span_ptp = np.ptp(span_data) # 极差
-            span_std = np.std(span_data) # 标准差
-            span_25 = np.percentile(span_data, 25) # 计算25分位数
-            span_75 = np.percentile(span_data, 75) # 计算75分位数
+            span_mean = np.mean(span_data)
+            span_ptp = np.ptp(span_data)
+            span_std = np.std(span_data)
+            span_25 = np.percentile(span_data, 25)
+            span_75 = np.percentile(span_data, 75)
             values = [span, span_mean, span_ptp, span_std, span_25, span_75]
             trace_series[str(i)] = span_data
         else:
